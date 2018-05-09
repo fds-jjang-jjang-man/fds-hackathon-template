54eb27816075b4368987712a6a7dffc33fb80509
simon <ybi4841@gmail.com>
2018-05-09 17:10:32 +0900

    until_ramdom_number

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const number1El = document.querySelector('.number1')
+const number2El = document.querySelector(".number2")
+const number3El = document.querySelector(".number3")
+const goEl = document.querySelector('.go')
+const restartEl = document.querySelector('.restart')
+const answerEl = document.querySelector('.answer')
+
+class BaseBallGame {
+  // 상태
+   newNumber = [
+    Math.floor(Math.random() * 10),
+    Math.floor(Math.random() * 10),
+    Math.floor(Math.random() * 10)
+  ];
+
+  // 중복되지 않는 숫자 반환
+  randomNum() {
+    while (1) {
+      if (
+        this.newNumber[0] != this.newNumber[1] &&
+        this.newNumber[0] != this.newNumber[2] &&
+        this.newNumber[1] != this.newNumber[2]
+      ) {
+        return this.newNumber;
+      } else {
+        this.newNumber = [
+          Math.floor(Math.random() * 10),
+          Math.floor(Math.random() * 10),
+          Math.floor(Math.random() * 10)
+        ];
+      }
+    }
+  }
+  // 동작
+  // - 체크
+  // checker(){
+  //   const number = [1,2,3];
+  //   if(this.)
+
+  // }
+}
+
+const game = new BaseBallGame();
+answerEl.textContent = game.randomNum();
+
+
+number1El.addEventListener('keydown', e =>{
+
+return number2El;
+
+} )
