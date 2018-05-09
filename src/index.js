2e6adaea13add5932efb849f8e279eb26091ffcf
Kim Sanghyeok <skyee23@gmail.com>
2018-05-09 16:06:13 +0900

    class 생성

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import './index.scss';
+class BaseBallGame {
+  // 상태
+  newNumber = [Math.floor(Math.random()*10),
+               Math.floor(Math.random()*10),
+               Math.floor(Math.random()*10)
+              ];
+  
+  // 중복되지 않는 숫자 반환 
+  randomNum(){
+	while(1){
+	  if(this.newNumber[0] != this.newNumber[1] &&
+	     this.newNumber[0] != this.newNumber[2] &&
+	     this.newNumber[1] != this.newNumber[2]
+	  ){
+	    return this.newNumber;
+	  }
+	  else{
+	    this.newNumber = [Math.floor(Math.random()*10),Math.floor(Math.random()*10),Math.floor(Math.random()*10)]
+	  }
+	}
+	}
+  
 
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
-});
+  
+  // 동작
+  
+  // - 체크
+  
+}
+
+
+const t = new BaseBallGame();
+console.log(t.randomNum());
