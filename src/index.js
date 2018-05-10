74ce288e809458488b236ab94544e95051f0ea10
Kim Sanghyeok <skyee23@gmail.com>
2018-05-10 16:31:24 +0900

    html ,js 수정(number I/O 로직까지 구현)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
-const number1El = document.querySelector('.number1');
-const number2El = document.querySelector('.number2');
-const number3El = document.querySelector('.number3');
+const numberInputEl = document.getElementsByClassName("number-input");
+const numberOutputEl = document.getElementsByClassName("number-output");
 const goEl = document.querySelector('.go');
-const restartEl = document.querySelector('.restart');
-const answerEl = document.querySelector('.answer');
-=======
-const number1El = document.querySelector('.number1')
-const number2El = document.querySelector(".number2")
-const number3El = document.querySelector(".number3")
-const numbersEl = document.querySelector('.numbers')
-const goEl = document.querySelector('.go')
-const restartEl = document.querySelector('.restart')
-const answerEl = document.querySelector('.answer')
->>>>>>> ea41803d2aec36cc585e64dba5b68866b17e1b5e
 
 class BaseBallGame {
   // 상태
@@ -58,13 +45,10 @@ class BaseBallGame {
     return [this.ball,this.strike,this.out];
   }
 }
-<<<<<<< HEAD
-=======
 
-
-
-
-
-const game = new BaseBallGame();
-answerEl.textContent = game.randomNum();
->>>>>>> ea41803d2aec36cc585e64dba5b68866b17e1b5e
+goEl.addEventListener('click', e =>{
+  for(let i = 0; i<numberInputEl.length;i++){
+    numberOutputEl[i].textContent = numberInputEl[i].value;
+     numberInputEl[i].value = '';
+   }
+})
